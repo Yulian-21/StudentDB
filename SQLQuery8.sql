4865433e1b0994e7cf06675226ea0b4f2fc8686f
Yulian-21 <56035917+Yulian-21@users.noreply.github.com>
2022-01-31 19:12:52 +0200

    Add files via upload

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -93,3 +93,108 @@ From Library
 inner Join LibraryActivity
 on LibraryActivity.BookId = Library.LibraryId and LibraryActivity.DeliveryDate is not Null 
 order by DateDiff(day,  LibraryActivity.DateOfReceiving, LibraryActivity.DeliveryDate) desc
+
+select count(*) 
+from Students 
+inner join Groups 
+on Students.GroupId = Groups.GroupID
+where Groups.Name = 'PKK'
+
+select Groups.Name, count(*)
+from Groups 
+where Groups.GroupID = 2
+
+select Groups.Name, Count(*)
+from Students
+inner join Groups
+on Students.GroupId = Groups.GroupID
+where Groups.Name like 'P%'
+group by Groups.Name
+having count(*)>=2
+
+Select * 
+from Groups 
+inner join Department 
+on Groups.DepartmentId = Department.DepartmentID
+where DepartmentName not like 'Mathematical'
+
+
+select Students.StudentID, Students.Name, Students.GroupId
+from Students
+left join LibraryActivity
+on Students.StudentID = LibraryActivity.StudentId
+where LibraryActivity.DateOfReceiving is NULL
+
+select Groups.Name, count(*)
+from Students
+inner join Groups
+on Students.GroupId = Groups.GroupID
+Group by Groups.Name
+
+--select Department.DepartmentName, count(*)
+--from Department
+--inner join Groups
+--on Department.DepartmentID = Groups.DepartmentId
+--inner Join Students 
+--on Groups.GroupID = Students.GroupId
+--Group by Department.DepartmentName
+
+select Department.DepartmentName, count(*)
+from Students
+inner join Groups
+on Students.GroupId = Groups.GroupID 
+inner join Department
+on Groups.DepartmentId = Department.DepartmentID
+Group By Department.DepartmentName 
+
+
+Select avg(Library.LibraryId)
+from Library
+inner join LibraryActivity
+on LibraryActivity.BookId = Library.LibraryId
+
+
+select Top(1)Groups.Name, count(*)--4
+from LibraryActivity
+inner join Students
+on Students.StudentID = LibraryActivity.StudentId
+inner join  Groups
+on Students.GroupId = Groups.GroupID
+Group by Groups.Name
+order by count(*) asc
+
+
+
+select  Library.BookName, Groups.Name as GroupName, Students.Name as StudentName
+from Library
+inner join LibraryActivity
+on Library.LibraryId = LibraryActivity.BookId
+inner join Students
+on LibraryActivity.StudentId = Students.StudentID
+inner join Groups 
+on Groups.GroupID = Students.GroupId
+
+
+select TotalBooksPerGroup.GroupName, 1.0 * TotalBooksPerGroup.TotalBooksCount / TotalStodentsPerGroup.TotalStudentsCount as Average
+from
+(
+	select Groups.Name as GroupName, count(*) as TotalBooksCount
+	from LibraryActivity
+	inner join Library 
+	on LibraryActivity.BookId = Library.LibraryId
+	inner join Students
+	on LibraryActivity.StudentId = Students.StudentID
+	inner join Groups
+	on Groups.GroupID = Students.GroupId
+	group by Groups.Name
+) as TotalBooksPerGroup
+inner join
+(
+	select Groups.Name as GroupName, count(*) as TotalStudentsCount
+	from Students
+	inner join Groups
+	on Students.GroupId = Groups.GroupID
+	Group by Groups.Name
+) as TotalStodentsPerGroup
+on TotalBooksPerGroup.GroupName = TotalStodentsPerGroup.GroupName
+
