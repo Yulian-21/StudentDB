7fabd46d00fbf06bc5694f6e65fbe379a07a4e08
Yulian-21 <56035917+Yulian-21@users.noreply.github.com>
2022-02-01 19:05:37 +0200

    Add files via upload

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -8,6 +8,31 @@ DepartmentName nvarchar(50) Not null
 )
 go
 
+create table Curators
+(
+CuratorId int identity(1,1) not null,
+CuratorName nvarchar(100) not null,
+CuratorAge int not null
+)
+
+insert into Curators(CuratorName, CuratorAge) values
+('Name1','20'),
+('name2','25'),
+('name3','40'),
+('name4','35')
+
+alter table Curators
+add  constraint PK_Curators_CuratorID
+primary key clustered (CuratorID)
+
+insert into Curators(CuratorName, CuratorAge)
+select Curators.CuratorName, Curators.CuratorAge
+from Curators
+
+select distinct *
+from Curators
+where Curators.CuratorId % 4 = 0
+
 
 EXEC sp_rename 'Department.DeploymentName', 'DepartmentName';
 go
